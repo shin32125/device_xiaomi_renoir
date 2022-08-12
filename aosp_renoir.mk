4addb5182f167cf357c957839f244ef0fe7cb316
shin32125 <shintaga32123@gmail.com>
2022-08-12 12:37:59 +0900

    Update aosp_renoir.mk

--- aosp_renoir.mk
+++ aosp_renoir.mk
@@ -12,13 +12,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/renoir/device.mk)
 
 # Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := renoir
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Mi 11 Lite 5G
-PRODUCT_NAME := aosp_renoir
+PRODUCT_NAME := lineage_renoir
 
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
@@ -27,3 +27,42 @@ TARGET_BOOT_ANIMATION_RES := 1080
 
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
+# maintainer flag (one word only/no spacing)
+RICE_MAINTAINER := shin32
+
+# chipset flag (one word only/no spacing)
+RICE_CHIPSET := chipset
+
+# gapps build flag, if not defined build type is vanilla
+WITH_GMS := true
+
+# Sushi Bootanimation (only 720/1080p/1440 supported. if not defined, bootanimation is google bootanimation)
+SUSHI_BOOTANIMATION := 1080
+
+# Graphene Camera
+TARGET_BUILD_GRAPHENEOS_CAMERA := true
+
+# disable/enable blur support, default is false
+TARGET_ENABLE_BLUR := false
+
+# UDFPS ICONS/ANIMATIONS
+TARGET_HAS_UDFPS := true
+
+# Quick tap feature
+TARGET_SUPPORTS_QUICK_TAP := true
+
+# Memory properties
+# Tune these properties according to device perfconfigstore.xml(if available)/device capabilities.
+# These props are overridable
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.fw.bg_apps_limit?=48 \
+    persist.sys.fw.use_trim_settings?=true \
+    persist.sys.fw.empty_app_percent?=50 \
+    persist.sys.fw.trim_empty_percent?=100 \
+    persist.sys.fw.trim_cache_percent?=100 \
+    persist.sys.fw.trim_enable_memory?=2147483648 \
+    persist.sys.fw.bservice_age?=120000 \
+    persist.sys.fw.bservice_limit?=6 \
+    persist.sys.fw.bservice_enable?=true 
+    
+    
